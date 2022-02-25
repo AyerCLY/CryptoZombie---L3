016421ffd4b6d38970c40ea121c72aa1be52c8bc
Ayer CLY <95411578+AyerCLY@users.noreply.github.com>
2022-02-25 11:44:50 +0800

    Update ZombieFactoryL3.sol

--- ZombieFactoryL3.sol
+++ ZombieFactoryL3.sol
@@ -10,6 +10,8 @@ contract ZombieFactory is Ownable {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+// 1. Define `cooldownTime` here
+    uint cooldownTime = 1 days;
 
     struct Zombie {
         string name;
@@ -25,7 +27,8 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // 2. Update the following line:
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
